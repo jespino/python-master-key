631488f2650b08f588ef49daf16c38307e066722
Jesús Espino <jespinog@gmail.com>
2015-07-24 14:31:16 +0200

    Get the longsize into a variable

--- masterkey/__init__.py
+++ masterkey/__init__.py
@@ -1,10 +1,12 @@
 import ctypes
 from operator import xor
 
+longsize = ctypes.sizeof(ctypes.c_long)
+
 def open_class(klass):
-    int_flags = ctypes.c_long.from_address(id(klass) + ctypes.sizeof(ctypes.c_long) * 21)
+    int_flags = ctypes.c_long.from_address(id(klass) + longsize * 21)
     int_flags.value |= 1 << 9
 
 def close_class(klass):
-    int_flags = ctypes.c_long.from_address(id(klass) + ctypes.sizeof(ctypes.c_long) * 21)
+    int_flags = ctypes.c_long.from_address(id(klass) + longsize * 21)
     int_flags.value = xor(int_flags.value, 1 << 9)
