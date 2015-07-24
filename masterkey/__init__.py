2bbaba993f4ab3d105fa040915f198395ec1521c
Jesús Espino <jespinog@gmail.com>
2015-07-24 12:37:14 +0200

    Allowing to close classes

--- masterkey/__init__.py
+++ masterkey/__init__.py
@@ -1,5 +1,10 @@
 import ctypes
+from operator import xor
 
 def open_class(klass):
     int_flags = ctypes.c_long.from_address(id(klass) + ctypes.sizeof(ctypes.c_long) * 21)
     int_flags.value |= 1 << 9
+
+def close_class(klass):
+    int_flags = ctypes.c_long.from_address(id(klass) + ctypes.sizeof(ctypes.c_long) * 21)
+    int_flags.value = xor(int_flags.value, 1 << 9)
